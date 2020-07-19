10db78625abe5758da8a64d2009c4908ca19cf3a
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2020-07-19 19:07:45 +0200

    check2 added

---
+++ tests/check2.py
@@ -0,0 +1,21 @@
+import simplio
+import numpy, os
+
+fname='test'
+
+f1=simplio.file(fname)
+f1.save('val1',1.234)
+dat=numpy.random.rand(400)
+f1.save('dir1/field',numpy.reshape(dat,(40,10)))
+
+f1.cd('dir1')
+
+dat=numpy.random.rand(500)
+f1.save('dir2/field',numpy.reshape(dat,(5,10,10)))
+
+f1.ls()
+
+out=os.popen(f'less {fname}.index.simplio').read()
+print(out)
+
+os.popen(f'rm {fname}.*')
