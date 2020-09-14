958fb78aad6e38f7b8288aa5062654e83ce8519b
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2020-09-15 00:53:08 +0200

    checks improved

--- tests/check1.py
+++ tests/check1.py
@@ -8,14 +8,17 @@ f1.save('val1',1.234)
 f1.save('cnfg1000/field1',numpy.random.rand(400))
 f1.save('cnfg1000/field2',numpy.random.rand(400))
 f1.save('cnfg1020/field1',numpy.random.rand(400))
-f1.save('cnfg1020/field2',numpy.random.rand(400))
+data = numpy.random.rand(400)
+f1.save('cnfg1020/field2',data)
 
 f2=simplio.file(fname)
 f2.ls()
 print(f2.load('val1'))
+assert f2.load('val1')==1.234
 
 dat=f2.load()
 print(dat['cnfg1000']['field1'])
+assert numpy.all(dat['cnfg1020']['field2']==data)
 
 f3=simplio.file(fname)
 f3.save('val1',1.456)
--- tests/check2.py
+++ tests/check2.py
@@ -18,4 +18,7 @@ f1.ls()
 out=os.popen(f'less {fname}.index.simplio').read()
 print(out)
 
+f2=simplio.file(fname)
+assert numpy.all(f2.load('dir1/dir2/field')==numpy.reshape(dat,(5,10,10)))
+
 os.popen(f'rm {fname}.*')
